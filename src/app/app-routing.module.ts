7df14fbf690bbe1501e982b75db3ed10757aeb68
Dominik <dominiksar92@gmail.com>
2019-03-19 09:06:41 +0100

    Add simple route animation
    Checkout new branch on git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,21 +8,22 @@ import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { InterestsComponent } from './interests/interests.component';
 import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'school', component: SchoolComponent},
-  { path: 'experiance', component: ExperienceComponent},
-  { path: 'skills', component: SkillsComponent},
-  { path: 'projects', component: ProjectsComponent},
-  { path: 'interests', component: InterestsComponent },
-  { path: 'contact', component: ContactComponent}
+  { path: '', component: HomeComponent, data: {animation: 'HomePage'}},
+  { path: 'about', component: AboutComponent, data: {animation: 'AboutPage'}},
+  { path: 'school', component: SchoolComponent, data: {animation: 'SchoolPage'}},
+  { path: 'experiance', component: ExperienceComponent, data: {animation: 'ExperiancePage'}},
+  { path: 'skills', component: SkillsComponent, data: {animation: 'SkillsPage'}},
+  { path: 'projects', component: ProjectsComponent, data: {animation: 'ProjectsPage'}},
+  { path: 'interests', component: InterestsComponent, data: {animation: 'InterestsPage'} },
+  { path: 'contact', component: ContactComponent, data: {animation: 'ContactPage'}}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), BrowserAnimationsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component , OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { slideInAnimation } from '../app/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    slideInAnimation
+    // animation triggers go here
+  ]
 })
 
 export class AppComponent {
   title = 'Portfolio';
   href = false;
+
+  // 
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
